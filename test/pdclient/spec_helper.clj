ac506869436498c01315fb7be83a411c7816d7a6
Daniel Ribeiro <danielrb@pagerduty.com>
2013-03-04 00:06:33 -0800

    a bit more test code cleanup

--- test/pdclient/spec_helper.clj
+++ test/pdclient/spec_helper.clj
@@ -1,25 +1,29 @@
 (ns pdclient.spec-helper
+  (:require  [clojure.string :as st])
   (:use clojure.test))
 
 ; Test Helpers
 (defn same [x y] (is (= x y)))
 
+
+
 (defn- name-to-symbol [name]
   (-> name
-    clojure.string/lower-case
-    (clojure.string/replace #"\W" "-")
-    (clojure.string/replace #"-+" "-")
-    (clojure.string/replace #"-$" "")
-    symbol)
-  )
+    st/lower-case
+    (st/replace #"\W" "-")
+    (st/replace #"-+" "-")
+    (st/replace #"-$" "")
+    symbol))
+
+(defn- arg-to-symbol [arg]
+  (if (symbol? arg) arg
+    (name-to-symbol arg)))
 
 ; Source: https://gist.github.com/mybuddymichael/4425558
 (defmacro spec
-  [name-string & body]
-    `(clojure.test/deftest ~(name-to-symbol name-string) ~@body))
+  [name & body]
+  `(clojure.test/deftest ~(arg-to-symbol name) ~@body))
 ; ignoring specs
 (defmacro xspec [name-string & body])
 
-;(defmacro describe [name & body] `(deftest ~name ~@body))
-; source: http://stackoverflow.com/questions/1317396/define-a-synonym-for-a-clojure-macro
-(def #^{:macro true} describe #'deftest)
+(defmacro describe [name & body] `(deftest ~(arg-to-symbol name) ~@body))
