9a1baae7a24ca7d55e0c4edd153608774c5af433
Daniel Ribeiro <danielrb@pagerduty.com>
2013-03-03 23:08:04 -0800

    starting to handle exceptions

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"] [clj-http "0.6.4"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http "0.6.4"]
+                 [cheshire "5.0.1"]
+                 ]
   :main pdclient.demo
   )
--- src/pdclient/demo.clj
+++ src/pdclient/demo.clj
@@ -1,27 +1,43 @@
 (ns pdclient.demo
   (:use pdclient.core)
+  (:require [clj-http.client])
+  (:require [cheshire.core :refer :all])
   )
-
-(user-delete :PGQFWUS)
-(grab (users) :id :email)
-(grab (services) :status)
-(user-new :role "user" :name "aguy from cljure" :email "him@example.com")
-(user-update :PGQFWUS :email "notreallyhim@example.com")
-
-
-(ns pdclient.demo)
-(use 'pdclient.core)
 (use 'clojure.pprint)
-(def p pprint)
 
-(defn pgrab [json & args]
-  (p (apply grab json args))
+(defn print-it [e]
+  (let [o (:object (.getData e ))]
+    (prn (:status o) (parse-string (o :body) true))
+    )
   )
+(try
+  (grab (users) :id :email)
+  (catch Exception e (print-it e) )
+)
 
-(pgrab (users) :id :email)
+(prn "what?")
 
-(user-delete :PGQFWUS)
-(pgrab (users) :id :email)
-(pgrab (services) :status)
-(user-new :role "user" :name "aguy from cljure" :email "him@example.com")
-(user-update :PGQFWUS :email "notreallyhim@example.com")
+;(user-delete :PGQFWUS)
+;(grab (users) :id :email)
+;(grab (services) :status)
+;(user-new :role "user" :name "aguy from cljure" :email "him@example.com")
+;(user-update :PGQFWUS :email "notreallyhim@example.com")
+;
+;
+;(ns pdclient.demo)
+;(use 'pdclient.core)
+;(use 'clojure.pprint)
+;(def p pprint)
+;
+;(defn pgrab [json & args]
+;  (p (apply grab json args))
+;  )
+;
+;(pgrab (users) :id :email)
+;
+;(user-delete :PGQFWUS)
+;(pgrab (users) :id :email)
+;(pgrab (services) :status)
+;(user-new :role "user" :name "aguy from cljure" :email "him@example.com")
+;(user-update :PGQFWUS :email "notreallyhim@example.com")
+;)
