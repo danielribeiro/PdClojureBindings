2490ba2483add663c2d9b52f5973f9fb0b9f9ab0
Daniel Ribeiro <danielrb@pagerduty.com>
2013-03-16 21:46:06 -0700

    building towards the macro

--- src/pdclient/core.clj
+++ src/pdclient/core.clj
@@ -58,25 +58,6 @@
   ((->> path-list last singularize-keyword) json)
   )
 
-; DEPRECATED
-(defn pdlist
-  "usage example: (pdlist [:users])"
-  [path-list & args]
-  (simplitfy-list path-list (pdrequest :get path-list args)))
-
-(defn pdshow [path-list & args]
-  (simplify-single-result path-list (pdrequest :get path-list args)))
-
-(defn pdcreate [path-list & args]
-  (simplitfy-create path-list (pdrequest :post path-list args)))
-
-(defn pddelete [path-list & args]
-  (pdrequest :delete path-list args))
-
-(defn pdupdate [path-list & args]
-  (simplify-single-result path-list (pdrequest :put path-list args)))
-; /DEPRECATED
-
 (def simplify-case
     {'list simplitfy-list
      'show simplify-single-result
@@ -142,8 +123,7 @@
   ))
 
 
-(defn route-specs [route]   ;;TODO: handle crud special case
-  (mapcat (partial extract-routes route) (:routes route)))
+(defn route-specs [route] (mapcat (partial extract-routes route) (:routes route)))
 
 (defn- get-simplify-function [routespec]
   (if (list? (:route-spec routespec))
@@ -178,26 +158,6 @@
     (select-keys json args)
     (map #(select-keys % args) json)
     ))
-
-;;crud is equal to [(get nil) (get :id nil) (post nil) (put :id nil) (delete :id nil)]
-;;which equals to [list show create update delete]
-(def pd
-  '(
-     [incidents list update show (get count) (get :id log_entries)]
-     [alerts list]
-     [reports (get alerts_per_time) (get incidents_per_time)]
-     [schedules crud (get :id users ) (post preview) (get :id entries)
-      [overrides list create delete]]
-     [users crud (get :id log_entries)
-      [contact_methods crud]
-      [notification_rules crud]]
-     [log_entries list show]
-     [services crud (put :id disable) (put :id enable) (post :id regenerate_key)
-        [email-filters create update delete]]
-     [maintenance_windows crud]
-   )
-
-  )
 
 (defn partition-with [pred coll]
   [(filter pred coll) (remove pred coll)])
@@ -255,31 +215,47 @@
 
 
 ;; doc helper.
-(defn printroutes []
-  (let [vars (mapcat route-specs (mapcat linearize pd))]
-    (doseq [x vars]  (println (route-to-function-name x)) (prn x))
-  ))
+;(defn printroutes []
+;  (let [vars (mapcat route-specs (mapcat linearize pd))]
+;    (doseq [x vars]  (println (route-to-function-name x)) (prn x))
+;  ))
+
+; Create the methods
+
+;(defmacro defineall [form]
+;  (cons `do
+;    (map (fn [routespec]
+;           `(define-pd-api ~routespec))
+;      (mapcat route-specs (mapcat linearize (quote form)))
+;
+;      )
+;  ) )
+
+;(defmacro defineall [form]
+;     (def pd (mapcat route-specs (mapcat linearize (quote ~form)))))
+
+(defmacro defineall [form]
+  `(def pd (quote ~form)))
 
 
-(define-pd-api {:route-spec (get count) :route {:element incidents :parent nil :routes [(get count)]}} )
-(define-pd-api {:route-spec show :route {:element :users :parent nil :routes [show create update delete list (get :id log_entries)]}})
+(defineall (
+             [incidents list update show (get count) (get :id log_entries)]
+             [alerts list]
+             [reports (get alerts_per_time) (get incidents_per_time)]
+             [schedules crud (get :id users ) (post preview) (get :id entries)
+              [overrides list create delete]]
+             [users crud (get :id log_entries)
+              [contact_methods crud]
+              [notification_rules crud]]
+             [log_entries list show]
+             [services crud (put :id disable) (put :id enable) (post :id regenerate_key)
+              [email-filters create update delete]]
+             [maintenance_windows crud]
+             ) )
 
-(defn users [& args] (apply pdlist [:users] args))
-(defn incidents [& args] (apply pdlist [:incidents] args))
-(defn schedules [& args] (apply pdlist [:schedules] args))
-(defn services [& args] (apply pdlist [:services] args))
-(defn maintenance_windows [& args] (apply pdlist [:maintenance_windows] args))
-(defn user-new [& args] (apply pdcreate [:users] args))
-(defn user-delete [id & args] (apply pddelete [:users (name id)] args))
-(defn user-update [id & args] (apply pdupdate [:users (name id)] args))
 
 
 ; precursor of the creator of all functions
-;(defmacro defineall [args]
-;  (cons `do
-;    (map (fn [a]
-;           `(defn ~(symbol (str "x" a)) [] (println ~(str a)))) (map first args)
-;
-;      ))
-;  )
-;
+
+
+(prn pd)
