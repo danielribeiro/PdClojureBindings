3e4b9687c432a03488a5e0d5fb95df3a2ab4d97c
Daniel Ribeiro <danielrb@pagerduty.com>
2013-03-18 00:52:49 -0700

    adding basic debug helper

--- src/pdclient/basic_helpers.clj
+++ src/pdclient/basic_helpers.clj
@@ -40,6 +40,11 @@
 
 (def rest-vec (comp vec rest))
 
-(defn prit [arg] (do (prn arg) arg))
+; Debugging helper. Usage: (prit + 1 2) which is simlar to (prn (+ 1 2)), but returns 3, instead of nil
+(defmacro prit [& form]
+  `(let [res# (~@form)]
+     (prn res#)
+     res#))
+
 
 (defn dasherize [s] (string/replace s "_" "-"))
