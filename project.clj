5dabab00f91d4388edccab5756175b8825671b8e
Daniel Ribeiro <danielrb@pagerduty.com>
2013-03-18 23:04:54 -0700

    preparing for jar deploy

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject pdclient "0.1.0"
   :description "pagerduty"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/danielribeiro/PdClojureBindings"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/mit-license.php"
+            :distribution :repo}
+  :warn-on-reflection false
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.6.4"]
                  [cheshire "5.0.1"]
