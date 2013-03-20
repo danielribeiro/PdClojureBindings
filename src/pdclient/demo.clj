f9d968d21a19868d0f954f3dfeb7feee9fa64cdd
Daniel Ribeiro <danielrb@pagerduty.com>
2013-03-19 22:48:40 -0700

    setting up demos

--- src/pdclient/demo.clj
+++ src/pdclient/demo.clj
@@ -1,46 +1,17 @@
 (ns pdclient.demo
-  (:use pdclient.core)
+  (:use pdclient.core clojure.pprint)
   (:require [clj-http.client])
-  (:require [cheshire.core :refer :all])
-  )
+  (:require [cheshire.core :refer :all]))
+
 
 (setup-auth {:subdomain "your-subdomain"
-             :user "your-username"
-             :password "your-password"
+             :token "your-accout-token"
              })
-(use 'clojure.pprint)
-(defn print-it [e]
-  (let [o (:object (.getData e ))]
-    (prn (:status o) (parse-string (o :body) true))
-    )
-  )
 
-;(try
-;  (grab (users) :id :email)
-;  (catch Exception e (print-it e) )
-;)
+; or
+;(setup-auth {:subdomain "your-subdomain"
+;             :user "your-username"
+;             :password "your-password"
+;             })
 
-;(user-delete :PGQFWUS)
-;(grab (users) :id :email)
-;(grab (services) :status)
-;(user-new :role "user" :name "aguy from cljure" :email "him@example.com")
-;(user-update :PGQFWUS :email "notreallyhim@example.com")
-;
-;
-;(ns pdclient.demo)
-;(use 'pdclient.core)
-;(use 'clojure.pprint)
-(def p pprint)
-;
-(defn pgrab [json & args]
-  (p (apply grab json args))
-  )
-;
-;(pgrab (users) :id :email)
-;
-;(user-delete :PGQFWUS)
-;(pgrab (users) :id :email)
-;(pgrab (services) :status)
-;(user-new :role "user" :name "aguy from cljure" :email "him@example.com")
-;(user-update :PGQFWUS :email "notreallyhim@example.com")
-;)
+
