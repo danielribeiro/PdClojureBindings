56faa3d8b3c1df9d045d567d3b34e7be69d369b6
Stefan du Fresne <stefan@sdufresne.info>
2015-09-05 16:48:50 +0100

    Update clj-http version
    This removes a compatibility issue with Clojure 1.7.0:
       WARNING: update already refers to: #'clojure.core/update in namespace: clj-http.client, being replaced by: #'clj-http.client/update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pdclient "0.1.3"
+(defproject pdclient "0.1.4"
   :description "pagerduty"
   :url "https://github.com/danielribeiro/PdClojureBindings"
   :license {:name "The MIT License"
@@ -7,7 +7,7 @@
   :warn-on-reflection false
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-http "1.1.2"]
+                 [clj-http "2.0.0"]
                  [cheshire "5.5.0"]
                  ]
   )
